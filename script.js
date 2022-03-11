f1100a57aadddb86b268ba004203b8fd04f76214
Seema <seema.n.thapliyal@gmail.com>
2022-03-10 21:28:53 -0600

    updated script.js with studio requirements

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+        .then(function(response) {
+            return response.json();
+        }).then(function(json) {
+        let sortedAstronauts = json.sort(function(a,b){return a.hoursInSpace-b.hoursInSpace});
+        let container = document.getElementById("container");
+        container.innerHtml +=`<h3>Astronaut Count: ${sortedAstronauts.length}</h3>`;
+        let allMyHtml = "";
+        for (let index = 0; index<json.length; index++){
+            let myHtml = `
+            <div class="astronaut">
+            <div class="bio">
+                <h3> ${json[index].firstName} ${json[index].lastName} </h3>
+                <ul>
+                <li>Hours in space: ${json[index].hoursInSpace}</li>
+                <li ${json[index].active? 'style="color:green;"' : ''}>Active: ${json[index].active}</li>
+                <li>Skills : ${json[index].skills}
+                </ul>
+                    <img class="avatar" src="${json[index].picture}"> </img>
+            </div>
+            </div>
+        `;
+        allMyHtml += myHtml;
+        }
+        container.innerHTML = allMyHtml;
+      });
+   });
